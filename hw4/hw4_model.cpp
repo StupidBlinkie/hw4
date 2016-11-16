e2ad90ca56ff29f335bcc3c4876d201a1b7bdf18
Yifan Gu <jackgu@uw.edu>
2016-11-16 00:23:11 -0800

    updates on class design
    no more memory leaks (still has reachable blocks though, might come from
    lib?? fix if have time)

--- hw4/hw4_model.cpp
+++ hw4/hw4_model.cpp
@@ -1,58 +1,53 @@
-//hw4_model
+//hw4_model  test gamedef & state class
+#include <vector>
 #include <iostream>
 #include "hw4_model.h"
 using namespace std;
 
 
-int*color;
-int*state;
-
 int main(){
 
+std::vector<int> color;
+std::vector<int> state;
+color = {0, 1, 2, 3, 4, 5,
+              0, 1, 2, 3, 4, 5,
+              0, 1, 2, 3};
+state = {1,1,1,1};
 
-  color = (int*)malloc(sizeof(int)*16);
-  for(int i = 0; i<16; i++){
-    color[i] = i;
-  }
+  gameDef g_def;
+  g_def.set_gameID(12345);
+  g_def.movesAllowed = 20;
+  g_def.colors = 6;
 
-  state = (int*)malloc(sizeof(int)*4);
-  for (int i =0; i<4;i++){
-      state[i] = 1;
-  }
+  
+  g_def.set_extensionColor(8, 2, color);
+  g_def.set_boardState(2,2, state);
 
+  cout << "id: " << g_def.gameID <<endl;
+  cout << "colors: " << g_def.colors <<endl;
+  cout << "moves allowed: " << g_def.movesAllowed <<endl;
 
- 	gameDef g_def;
- 	g_def.set_gameID(12345);
- 	g_def.movesAllowed = 20;
- 	g_def.colors = 6;
+  for (int r = 0; r<8; r++){
+    for (int c = 0; c<2; c++){
+      //cout << "reteriving row: " << r << " col: " << c <<endl;
+      void * temp = g_def.get_extensionColor_element(r, c);
+      cout << "data: " << *(int*)temp <<endl;
+    }
+  }   
 
-	
- 	g_def.set_extensionColor(8, 2, color);
- 	g_def.set_boardState(2,2, state);
-
- 	cout << "id: " << g_def.gameID <<endl;
- 	cout << "colors: " << g_def.colors <<endl;
- 	cout << "moves allowed: " << g_def.movesAllowed <<endl;
-
- 	for (int r = 0; r<8; r++){
- 		for (int c = 0; c<2; c++){
-		  //cout << "reteriving row: " << r << " col: " << c <<endl;
- 			void * temp = g_def.get_extensionColor_element(r, c);
-			cout << "data: " << *(int*)temp <<endl;
- 		}
- 	}		
-
- 	cout << "-----------" <<endl;
+  cout << "-----------" <<endl;
 
 
- 	for (int r = 0; r<2; r++){
- 		for (int c = 0; c<2; c++){
- 			void * temp = g_def.get_boardState_element(r, c);
-			cout << "boardState_data: " << *(int*)temp <<endl;
- 		}
- 	}	
+  for (int r = 0; r<2; r++){
+    for (int c = 0; c<2; c++){
+      void * temp = g_def.get_boardState_element(r, c);
+      cout << "boardState_data: " << *(int*)temp <<endl;
+    }
+  } 
 
- 	return 0;
+
+
+  return 0;
 
 }
 
--- hw4/hw4_model.h
+++ hw4/hw4_model.h
@@ -1,6 +1,9 @@
 #ifndef _HW4_MODEL_H_
 #define _HW4_MODEL_H_
+
+#include <vector>
 using namespace std;
+
  extern "C"{
 	#include "../hw2/array2d.h"
  }
@@ -8,7 +11,10 @@ using namespace std;
 
 class gameDef{
  public:
- 	//gameDefinition();
+
+ 	//std::vector<int> to store data from json read
+ 	//replace the global ones in model??
+
 
  	int gameID;
  	Array2dPtr extensionColor;
@@ -20,15 +26,19 @@ class gameDef{
 	void set_movesAllowed(int m) {movesAllowed = m;}
 	void set_colors(int c) {colors = c;}
  	
- 	void set_extensionColor(int rows, int cols, int* data_from_json);
- 	void set_boardState(int rows, int cols, int* data_from_json);
+ 	void set_extensionColor(int rows, int cols, std::vector<int> data_from_json);
+ 	void set_boardState(int rows, int cols, std::vector<int> data_from_json);
  	void* get_extensionColor_element(int row, int col);
  	void* get_boardState_element(int row, int col);
-	
+
+	void free_extensionColor();
+	void free_boardState();
+
+	~gameDef(void);
 };
 
 
-inline void gameDef::set_extensionColor(int rows, int cols, int* data_from_json){
+inline void gameDef::set_extensionColor(int rows, int cols,std::vector<int> data_from_json){
  	extensionColor = A2d_AllocateArray2d(rows, cols, sizeof(void*));
 	//fill 2d array
 	for (int r=0; r<rows; r++) {
@@ -38,7 +48,7 @@ inline void gameDef::set_extensionColor(int rows, int cols, int* data_from_json)
 	}
 }
 
-inline void gameDef::set_boardState(int rows, int cols, int* data_from_json){
+inline void gameDef::set_boardState(int rows, int cols,std::vector<int> data_from_json){
 	boardState = A2d_AllocateArray2d(rows, cols, sizeof(void*));
 	//fill 2d array
 	for (int r=0; r<rows; r++) {
@@ -54,6 +64,26 @@ inline void* gameDef::get_extensionColor_element(int row, int col){
 }
 inline void* gameDef::get_boardState_element(int row, int col){
 	return A2d_GetElement(boardState, row, col);
+}
+
+//free functions for desctructor, make a client free callback func?
+inline void gameDef::free_extensionColor(){
+  A2d_FreeArray2d(extensionColor, NULL);  
+  free(extensionColor->storage);
+  free(extensionColor);
+}
+inline void gameDef::free_boardState(){
+  A2d_FreeArray2d(extensionColor, NULL); 
+  free(boardState->storage);
+  free(boardState);
+}
+
+
+//destructor 
+inline gameDef::~gameDef(void){
+	cout << "Object is being deleted" << endl;
+	free_extensionColor();  
+	free_boardState();
 }
 
 
